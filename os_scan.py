4580b6948b9cb40a961704b11ec6e1d2241d30d2
user <user@Ubuntu-20.04-LTS.myguest.virtualbox.org>
2025-03-19 22:07:58 -0500

    Started implementation of Os_Scanner Class

--- os_scan.py
+++ os_scan.py
@@ -4,20 +4,42 @@ Scanner for gathering information from the operating system
 """
 
 import os
-import system
+import pwd
+import grp
+import stat
+import subprocess
 
 class OsScanner:
 
 	def __init__(self):
-		self.OsType = ""
-		self.OsUsers = []
-		self.OsGroups = []
-		self.OsServices = []
-		self.OsCurrentUser = []
+		self.OsType = os.name # Operating System Type 
+		self.OsUsers = [] # List of system users
+		self.OsGroups = [] # List of system groups
+		self.OsServices = [] # List of system services
+		self.OsCurrentUser = "" # Current user of the system
 
-	# Gather information about the user running the program 
-	def gatherUserInfo(self):
-		pass
+	# Get Current User function
+	def get_OsCurrentUser(self):
+		try:
+			current_user = os.getlogin() # Returns a string denoting the name of the current user
+		except Excpetion:
+			current_user = pwd.getpwuuid(os.getuid()).pw_name # If exception, try to get user name through password database
+
+		return current_user
+
+
+	def set_OsCurrentUser(seelf)	
+
+	# Get OsUsers List fuction
+	def get_OsUsers(self):
+		
+		if self.OsType == 'posix':
+			self.OsUsers = [user.pw_name for user in pwd.getpwall()]
+		else:
+			self.OsUsers = []
+
+		return self.OsUsers
+
 
 	def gatherGroupInfo(self):
 		pass
@@ -27,3 +49,14 @@ class OsScanner:
 
 	def checkBadPermissions(self):
 		pass
+
+
+if __name__ == "__main__":
+	scanner = OsScanner()
+	user_info = scanner.get_OsCurrentUser()
+	print("Current User:", user_info)
+
+	users = scanner.get_OsUsers()
+	print("Current User List:")
+	for user in users:
+		print("\t\t  " + user)
