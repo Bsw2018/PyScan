8e509495dda4e8dda85520947e4925cae183b576
user <user@Ubuntu-20.04-LTS.myguest.virtualbox.org>
2025-03-21 18:45:55 -0500

    (3/19/25) Commit #3
    Added two classes that are members of OsScanner:
    
    	User: Represents a User of the system with six attributes
    	      - username : str
    	      - hasPaswword : bool
                  - userid : str
    	      - groupid : str
    	      - home_dir : str
    	      - shell : str
    
    	Group: Represents a group of the system with four attributes
    	      - groupname : str
                  - hasPassword : bool
                  - group_id : str
                  - members : list of strings
    
    Added python virtual environment for installing pip packages that are listed in the requirements.txt
    
    Changed the get_OsUsers function that is part of the OsScanner class. The get_OsUsers function is now responsible for populating
    the OsUsers member list with User objects.
    
    Changed the get_OsGroups function that is part of the OsScanner class. The get_OsGroups function will now populate the OsGroups member
    with Group objects.
    
    Created a Tkinter widget with the data returned from the get_OsUsers function. The root widget contains a notebook widget for adding tabs
    with the first tab being a Tree widget. The OsUsers Tree widget has six columns represented by the member variables of the User class.
    
    Need to put all the GUI statments in a class to clean up the code

--- os_scan.py
+++ os_scan.py
@@ -8,6 +8,11 @@ import pwd
 import grp
 import stat
 import subprocess
+from User import User
+import tkinter as tk
+from tkinter import ttk
+from Group import Group
+
 
 class OsScanner:
 
@@ -35,7 +40,8 @@ class OsScanner:
 	def get_OsUsers(self):
 		
 		if self.OsType == 'posix':
-			self.OsUsers = [user.pw_name for user in pwd.getpwall()]
+			for user in pwd.getpwall():
+				self.OsUsers.append(User(user.pw_name, user.pw_passwd, user.pw_uid, user.pw_gid, user.pw_dir, user.pw_shell))
 		else:
 			self.OsUsers = []
 
@@ -48,9 +54,10 @@ class OsScanner:
 		except Excpetion:
 			groups = []
 
-		#self.OsGroups = groups
+		for group in groups:
+			self.OsGroups.append(Group(group.gr_name, group.gr_passwd, group.gr_gid, group.gr_mem))
 
-		return groups
+		return self.OsGroups
 		
 
 	def gatherFileSystem(self):
@@ -64,12 +71,87 @@ if __name__ == "__main__":
 	scanner = OsScanner()
 	
 	# Tester for get_OsCurrentUser functions
-	name = scanner.get_OsCurrentUser()
-	print("Current User:", name)
+	#name = scanner.get_OsCurrentUser()
+	#print("Current User:", name)
+
+	root = tk.Tk()
+	root.title("User Table")
+	root.geometry("800x1800")
+
+	notebook = ttk.Notebook(root)
+	notebook.pack(expand=True, fill="both")
+
+	table_frame = ttk.Frame(notebook)
+	notebook.add(table_frame, text="User Table")
+
+
+	style = ttk.Style(table_frame)
+	style.configure("Treeview",
+					background="#F0F0F0",
+					foreground="black",
+					rowheight=25,
+					fieldbackground="#F0F0F0")
+	style.configure("Treeview.Heading",
+					background="#4a4a4a",
+					foreground="white",
+					relief="flat")
+
+	columns = ("username", "password", "uid", "gid", "home_dir", "shell")
+	tree = ttk.Treeview(table_frame, columns=columns, show="headings")
+
+	for col in columns:
+		tree.heading(col, text=col.capitalize(), anchor="center")
+		tree.column(col, anchor="center", width=100)
+
+	tree.heading("username", text="Username")
+	tree.heading("password", text="Password")
+	tree.heading("uid", text="UID")
+	tree.heading("gid", text="GID")
+	tree.heading("home_dir", text="Home Directory")
+	tree.heading("shell", text="Shell")
+
+	tree.column("username", width=100)
+	tree.column("uid", width=50)
+	tree.column("gid", width=50)
+	tree.column("home_dir", width=150)
+	tree.column("shell", width=100)
+
+
+	for index,user in enumerate(scanner.get_OsUsers()):
+		
+		tag = "evenrow" if index % 2 == 0 else "oddrow"
+
+		tree.insert("","end" , values=(
+			user.username, 
+			user.hasPassword, 
+			user.userid, 
+			user.groupid, 
+			user.home_dir, 
+			user.shell), tags=(tag,))
+
+
+	tree.tag_configure("evenrow", background="#E8E8E8")
+	tree.tag_configure("oddrow", background="#DFDFDF")
+
+
+	tree.pack(expand=True, fill="both")
+
+
+	root.mainloop()
+
+
+	#Tester for get_OsUsers:
+	
+
 
 	# Tester for get_OsGroups() function
 	groups = scanner.get_OsGroups() # Returns a struct of a group {gr_name, gr_passwd, gr_gid, gr_mem} 
 									# Group Name, Group password, Numerical Group ID, Group Member's User names
 	print("Current Groups:")
-	for group in groups:
-		print(group.gr_name + "\t" + str(group.gr_gid)) # Print each group name and group id
+	for group in scanner.get_OsGroups():
+		print(group.name + str(group.hasPassword) + str(group.group_id), end="") # Print each group name and group id
+		for member in enumerate(group.members):
+			print(member)
+		print()
+
+
